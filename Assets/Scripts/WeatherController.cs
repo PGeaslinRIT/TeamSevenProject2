373881c9d6d21304a9b1989d285e9692b7b2738c
PGeaslinRIT <pxg6350@rit.edu>
2018-10-23 10:02:24 -0400

    Basic Obstacles
    Added Basic Obstacles and boundary walls
    Added a countdown to end wind. Wind is still unwieldy

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class WeatherController : MonoBehaviour {
 	public int temperature = 0;
 	public int precipitation = 0;
+
+	//wind variables
 	public int windSpeed = 0;
 	private bool windIncrease = true;
 	public Vector3 windDir = Vector3.zero;
 	public Vector3 windForce = Vector3.zero;
-
+	public int windMax = 0;
 	public bool awaitWindCapture = false;
+
+	public int timePassed = 0;
+
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	// Update once per frame
 	void Update () {
+		//collect data from clicking
 		if (Input.GetMouseButtonDown(0)) {
+			//wind direction
 			if (awaitWindCapture) {
 				windDir = gameObject.transform.FindChild ("MainCamera").transform.forward;
 				if (windIncrease) {
 					windSpeed++;
+					windMax += 250;
 				} else if (windSpeed > 0) {
 					windSpeed--;
 				}
@@ -31,7 +39,18 @@ public class WeatherController : MonoBehaviour {
 			}
 		}
 
+		//decrease how long wind lasts
+		if (windMax > 0) {
+			windMax--;
+
+			if (windMax == 0) {
+				windSpeed--;
+			}
+		}
+
 		windForce = windDir * windSpeed;
+
+		timePassed++;
 	}
 
 	//methods to change weather tied to bones
